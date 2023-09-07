e314fd5bf1b0690b7d6e7215d483968f31ff9961
PC <milagrosnavarro1d@gmail.com>
2023-09-07 00:33:04 -0300

    cambios en el inicio de admin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,59 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.Bproductos = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.Bventas = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.Bempleados = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.Bclientes = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(151, 243);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 41);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Clientes";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(506, 507);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 41);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Productos";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(176, 507);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 41);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Ventas";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(480, 243);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(167, 41);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Empleados";
             // 
             // Bproductos
             // 
@@ -90,11 +48,22 @@
             this.Bproductos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.Bproductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Bproductos.Image = global::SaborAcielo.Properties.Resources.EmpleadosInicio;
-            this.Bproductos.Location = new System.Drawing.Point(446, 273);
+            this.Bproductos.Location = new System.Drawing.Point(165, 280);
             this.Bproductos.Name = "Bproductos";
-            this.Bproductos.Size = new System.Drawing.Size(252, 243);
+            this.Bproductos.Size = new System.Drawing.Size(237, 234);
             this.Bproductos.TabIndex = 3;
             this.Bproductos.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(218, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 41);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Productos";
             // 
             // Bventas
             // 
@@ -103,11 +72,22 @@
             this.Bventas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.Bventas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Bventas.Image = global::SaborAcielo.Properties.Resources.VentasInicio;
-            this.Bventas.Location = new System.Drawing.Point(434, 5);
+            this.Bventas.Location = new System.Drawing.Point(558, 8);
             this.Bventas.Name = "Bventas";
             this.Bventas.Size = new System.Drawing.Size(242, 235);
             this.Bventas.TabIndex = 2;
             this.Bventas.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(624, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ventas";
             // 
             // Bempleados
             // 
@@ -116,11 +96,22 @@
             this.Bempleados.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.Bempleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Bempleados.Image = global::SaborAcielo.Properties.Resources.ProductosInicio;
-            this.Bempleados.Location = new System.Drawing.Point(82, 273);
+            this.Bempleados.Location = new System.Drawing.Point(165, -7);
             this.Bempleados.Name = "Bempleados";
-            this.Bempleados.Size = new System.Drawing.Size(260, 249);
+            this.Bempleados.Size = new System.Drawing.Size(234, 250);
             this.Bempleados.TabIndex = 1;
             this.Bempleados.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(624, 518);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Clientes";
             // 
             // Bclientes
             // 
@@ -129,42 +120,66 @@
             this.Bclientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.Bclientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Bclientes.Image = global::SaborAcielo.Properties.Resources.ClientesInicio;
-            this.Bclientes.Location = new System.Drawing.Point(64, 12);
+            this.Bclientes.Location = new System.Drawing.Point(567, 280);
             this.Bclientes.Name = "Bclientes";
-            this.Bclientes.Size = new System.Drawing.Size(310, 228);
+            this.Bclientes.Size = new System.Drawing.Size(233, 235);
             this.Bclientes.TabIndex = 0;
             this.Bclientes.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(218, 518);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Empleados";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.Bclientes);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Bventas);
+            this.panel1.Controls.Add(this.Bproductos);
+            this.panel1.Controls.Add(this.Bempleados);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(897, 599);
+            this.panel1.TabIndex = 8;
             // 
             // FinicioAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SaborAcielo.Properties.Resources.fondoSACinicio;
-            this.ClientSize = new System.Drawing.Size(800, 557);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Bproductos);
-            this.Controls.Add(this.Bventas);
-            this.Controls.Add(this.Bempleados);
-            this.Controls.Add(this.Bclientes);
+            this.ClientSize = new System.Drawing.Size(897, 599);
+            this.Controls.Add(this.panel1);
             this.Name = "FinicioAdmin";
             this.Text = "Inicio Administrador";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Bclientes;
-        private System.Windows.Forms.Button Bempleados;
-        private System.Windows.Forms.Button Bventas;
         private System.Windows.Forms.Button Bproductos;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Bventas;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Bempleados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Bclientes;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace SaborAcielo
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
