7476528cca2a57b7a339f60f7ad9c37db533b1f6
PC <milagrosnavarro1d@gmail.com>
2023-09-06 22:42:37 -0300

    Cambios inicio admin

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SaborAcielo
+{
+    partial class FinicioAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Bproductos = new System.Windows.Forms.Button();
+            this.Bventas = new System.Windows.Forms.Button();
+            this.Bempleados = new System.Windows.Forms.Button();
+            this.Bclientes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(151, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(506, 507);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 41);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Productos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(176, 507);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ventas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Script MT Bold", 25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(480, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Empleados";
+            // 
+            // Bproductos
+            // 
+            this.Bproductos.BackColor = System.Drawing.Color.Transparent;
+            this.Bproductos.FlatAppearance.BorderSize = 0;
+            this.Bproductos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Bproductos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Bproductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bproductos.Image = global::SaborAcielo.Properties.Resources.EmpleadosInicio;
+            this.Bproductos.Location = new System.Drawing.Point(446, 273);
+            this.Bproductos.Name = "Bproductos";
+            this.Bproductos.Size = new System.Drawing.Size(252, 243);
+            this.Bproductos.TabIndex = 3;
+            this.Bproductos.UseVisualStyleBackColor = false;
+            // 
+            // Bventas
+            // 
+            this.Bventas.BackColor = System.Drawing.Color.Transparent;
+            this.Bventas.FlatAppearance.BorderSize = 0;
+            this.Bventas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Bventas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bventas.Image = global::SaborAcielo.Properties.Resources.VentasInicio;
+            this.Bventas.Location = new System.Drawing.Point(434, 5);
+            this.Bventas.Name = "Bventas";
+            this.Bventas.Size = new System.Drawing.Size(242, 235);
+            this.Bventas.TabIndex = 2;
+            this.Bventas.UseVisualStyleBackColor = false;
+            // 
+            // Bempleados
+            // 
+            this.Bempleados.BackColor = System.Drawing.Color.Transparent;
+            this.Bempleados.FlatAppearance.BorderSize = 0;
+            this.Bempleados.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Bempleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bempleados.Image = global::SaborAcielo.Properties.Resources.ProductosInicio;
+            this.Bempleados.Location = new System.Drawing.Point(82, 273);
+            this.Bempleados.Name = "Bempleados";
+            this.Bempleados.Size = new System.Drawing.Size(260, 249);
+            this.Bempleados.TabIndex = 1;
+            this.Bempleados.UseVisualStyleBackColor = false;
+            // 
+            // Bclientes
+            // 
+            this.Bclientes.BackColor = System.Drawing.Color.Transparent;
+            this.Bclientes.FlatAppearance.BorderSize = 0;
+            this.Bclientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Bclientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bclientes.Image = global::SaborAcielo.Properties.Resources.ClientesInicio;
+            this.Bclientes.Location = new System.Drawing.Point(64, 12);
+            this.Bclientes.Name = "Bclientes";
+            this.Bclientes.Size = new System.Drawing.Size(310, 228);
+            this.Bclientes.TabIndex = 0;
+            this.Bclientes.UseVisualStyleBackColor = false;
+            // 
+            // FinicioAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SaborAcielo.Properties.Resources.fondoSACinicio;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bproductos);
+            this.Controls.Add(this.Bventas);
+            this.Controls.Add(this.Bempleados);
+            this.Controls.Add(this.Bclientes);
+            this.Name = "FinicioAdmin";
+            this.Text = "Inicio Administrador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bclientes;
+        private System.Windows.Forms.Button Bempleados;
+        private System.Windows.Forms.Button Bventas;
+        private System.Windows.Forms.Button Bproductos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace SaborAcielo.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ClientesInicio {
+            get {
+                object obj = ResourceManager.GetObject("ClientesInicio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cruz {
             get {
                 object obj = ResourceManager.GetObject("cruz", resourceCulture);
@@ -83,9 +93,79 @@ namespace SaborAcielo.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap decoracion_de_pasteles {
+            get {
+                object obj = ResourceManager.GetObject("decoracion-de-pasteles", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap EmpleadosInicio {
+            get {
+                object obj = ResourceManager.GetObject("EmpleadosInicio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fondoSACinicio {
+            get {
+                object obj = ResourceManager.GetObject("fondoSACinicio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ProductosInicio {
+            get {
+                object obj = ResourceManager.GetObject("ProductosInicio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap saboracielo2 {
             get {
                 object obj = ResourceManager.GetObject("saboracielo2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap save_money_1611179 {
+            get {
+                object obj = ResourceManager.GetObject("save-money_1611179", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap teamwork_2502447 {
+            get {
+                object obj = ResourceManager.GetObject("teamwork_2502447", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap VentasInicio {
+            get {
+                object obj = ResourceManager.GetObject("VentasInicio", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
