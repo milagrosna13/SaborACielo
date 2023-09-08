e4cc797df1ecedeaa0c0c29fa404210822fe0e90
PC <milagrosnavarro1d@gmail.com>
2023-09-08 11:35:18 -0300

    Formulario lista de productos

---
+++ Fproducto.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SaborAcielo
+{
+    partial class Fproducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GBfiltroProductos = new System.Windows.Forms.GroupBox();
+            this.Lnombre = new System.Windows.Forms.Label();
+            this.ltipo = new System.Windows.Forms.Label();
+            this.CBtodosProductos = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.GBfiltroProductos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(53, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(714, 285);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // GBfiltroProductos
+            // 
+            this.GBfiltroProductos.Controls.Add(this.CBtodosProductos);
+            this.GBfiltroProductos.Controls.Add(this.ltipo);
+            this.GBfiltroProductos.Controls.Add(this.Lnombre);
+            this.GBfiltroProductos.Location = new System.Drawing.Point(53, 12);
+            this.GBfiltroProductos.Name = "GBfiltroProductos";
+            this.GBfiltroProductos.Size = new System.Drawing.Size(714, 100);
+            this.GBfiltroProductos.TabIndex = 1;
+            this.GBfiltroProductos.TabStop = false;
+            this.GBfiltroProductos.Text = "Filtrar por";
+            // 
+            // Lnombre
+            // 
+            this.Lnombre.AutoSize = true;
+            this.Lnombre.Location = new System.Drawing.Point(287, 20);
+            this.Lnombre.Name = "Lnombre";
+            this.Lnombre.Size = new System.Drawing.Size(44, 13);
+            this.Lnombre.TabIndex = 0;
+            this.Lnombre.Text = "Nombre";
+            // 
+            // ltipo
+            // 
+            this.ltipo.AutoSize = true;
+            this.ltipo.Location = new System.Drawing.Point(494, 20);
+            this.ltipo.Name = "ltipo";
+            this.ltipo.Size = new System.Drawing.Size(28, 13);
+            this.ltipo.TabIndex = 1;
+            this.ltipo.Text = "Tipo";
+            // 
+            // CBtodosProductos
+            // 
+            this.CBtodosProductos.AutoSize = true;
+            this.CBtodosProductos.Location = new System.Drawing.Point(19, 57);
+            this.CBtodosProductos.Name = "CBtodosProductos";
+            this.CBtodosProductos.Size = new System.Drawing.Size(122, 17);
+            this.CBtodosProductos.TabIndex = 2;
+            this.CBtodosProductos.Text = "Todos los productos";
+            this.CBtodosProductos.UseVisualStyleBackColor = true;
+            // 
+            // Fproducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GBfiltroProductos);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Fproducto";
+            this.Text = "Lista de Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.GBfiltroProductos.ResumeLayout(false);
+            this.GBfiltroProductos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox GBfiltroProductos;
+        private System.Windows.Forms.Label ltipo;
+        private System.Windows.Forms.Label Lnombre;
+        private System.Windows.Forms.CheckBox CBtodosProductos;
+    }
+}
