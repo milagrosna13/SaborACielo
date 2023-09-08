837088ee233a0ddf9a5383309b68587d094d2d05
PC <milagrosnavarro1d@gmail.com>
2023-09-08 10:54:35 -0300

    mdi recuperado

---
+++ FagregarVenta.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SaborAcielo
+{
+    partial class FagregarVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(100, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 360);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 422);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1089, 223);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "aca mostrar lo que agrega al \"carrito\"";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Finalizar Compra";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(28, 141);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(795, 108);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Productos";
+            // 
+            // FagregarVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 657);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FagregarVenta";
+            this.Text = "FagregarVenta";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ MDIempleado.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SaborAcielo
+{
+    partial class MDIempleado
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.Bventa = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.Bproducto = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.BverC = new System.Windows.Forms.Button();
+            this.BagregarC = new System.Windows.Forms.Button();
+            this.Bcliente = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.Bventa);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.Bproducto);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.Bcliente);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(221, 453);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button10);
+            this.panel5.Controls.Add(this.button11);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Font = new System.Drawing.Font("Script MT Bold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel5.Location = new System.Drawing.Point(0, 330);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(221, 71);
+            this.panel5.TabIndex = 6;
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.Location = new System.Drawing.Point(0, 34);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(221, 34);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Listar";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button11.Location = new System.Drawing.Point(0, 0);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(221, 34);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "Generar ventas";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Bventa
+            // 
+            this.Bventa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Bventa.Font = new System.Drawing.Font("Script MT Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.Bventa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bventa.Location = new System.Drawing.Point(0, 296);
+            this.Bventa.Margin = new System.Windows.Forms.Padding(2);
+            this.Bventa.Name = "Bventa";
+            this.Bventa.Size = new System.Drawing.Size(221, 34);
+            this.Bventa.TabIndex = 5;
+            this.Bventa.Text = "Ventas";
+            this.Bventa.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button7);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Font = new System.Drawing.Font("Script MT Bold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel4.Location = new System.Drawing.Point(0, 261);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(221, 35);
+            this.panel4.TabIndex = 4;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(0, 0);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(221, 34);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Listar productos";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Bproducto
+            // 
+            this.Bproducto.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Bproducto.Font = new System.Drawing.Font("Script MT Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.Bproducto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bproducto.Location = new System.Drawing.Point(0, 227);
+            this.Bproducto.Margin = new System.Windows.Forms.Padding(2);
+            this.Bproducto.Name = "Bproducto";
+            this.Bproducto.Size = new System.Drawing.Size(221, 34);
+            this.Bproducto.TabIndex = 3;
+            this.Bproducto.Text = "Productos";
+            this.Bproducto.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.BverC);
+            this.panel3.Controls.Add(this.BagregarC);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Font = new System.Drawing.Font("Script MT Bold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(0, 160);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(221, 67);
+            this.panel3.TabIndex = 2;
+            // 
+            // BverC
+            // 
+            this.BverC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BverC.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BverC.Location = new System.Drawing.Point(0, 34);
+            this.BverC.Margin = new System.Windows.Forms.Padding(2);
+            this.BverC.Name = "BverC";
+            this.BverC.Size = new System.Drawing.Size(221, 31);
+            this.BverC.TabIndex = 4;
+            this.BverC.Text = "Listar clientes";
+            this.BverC.UseVisualStyleBackColor = true;
+            // 
+            // BagregarC
+            // 
+            this.BagregarC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BagregarC.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BagregarC.Location = new System.Drawing.Point(0, 0);
+            this.BagregarC.Margin = new System.Windows.Forms.Padding(2);
+            this.BagregarC.Name = "BagregarC";
+            this.BagregarC.Size = new System.Drawing.Size(221, 34);
+            this.BagregarC.TabIndex = 2;
+            this.BagregarC.Text = "Agregar cliente";
+            this.BagregarC.UseVisualStyleBackColor = true;
+            this.BagregarC.Click += new System.EventHandler(this.BagregarC_Click);
+            // 
+            // Bcliente
+            // 
+            this.Bcliente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Bcliente.Font = new System.Drawing.Font("Script MT Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.Bcliente.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bcliente.Location = new System.Drawing.Point(0, 126);
+            this.Bcliente.Margin = new System.Windows.Forms.Padding(2);
+            this.Bcliente.Name = "Bcliente";
+            this.Bcliente.Size = new System.Drawing.Size(221, 34);
+            this.Bcliente.TabIndex = 1;
+            this.Bcliente.Text = "Clientes";
+            this.Bcliente.UseVisualStyleBackColor = true;
+            this.Bcliente.Click += new System.EventHandler(this.Bcliente_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(221, 126);
+            this.panel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cerrar Sesión";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MDIempleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.Controls.Add(this.panel1);
+            this.IsMdiContainer = true;
+            this.Name = "MDIempleado";
+            this.Text = "MDIempleado";
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button Bventa;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button Bproducto;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button BverC;
+        private System.Windows.Forms.Button BagregarC;
+        private System.Windows.Forms.Button Bcliente;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
+
+
