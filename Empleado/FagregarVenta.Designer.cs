48bf31562e96aef181321de00542725ca1698462
LourdesRod <lourd.rodoj@gmail.com>
2023-09-13 21:55:25 -0300

    formulario de ventas

--- Empleado/FagregarVenta.Designer.cs
+++ Empleado/FagregarVenta.Designer.cs
@@ -29,103 +29,222 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Bcompra = new System.Windows.Forms.Button();
+            this.BcancelarCompra = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
+            this.Ncant = new System.Windows.Forms.NumericUpDown();
+            this.CBtipoProd = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CBproducto = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBprecioProd = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BagregarCompra = new System.Windows.Forms.Button();
+            this.DGcarrito = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ncant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGcarrito)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.dataGridView2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.Controls.Add(this.BagregarCompra);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.TBprecioProd);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.CBproducto);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.CBtipoProd);
+            this.panel1.Controls.Add(this.Ncant);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(100, 25);
+            this.panel1.Location = new System.Drawing.Point(52, 25);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(865, 360);
+            this.panel1.Size = new System.Drawing.Size(959, 366);
             this.panel1.TabIndex = 0;
             // 
-            // dataGridView2
+            // Bcompra
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(28, 141);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 62;
-            this.dataGridView2.RowTemplate.Height = 28;
-            this.dataGridView2.Size = new System.Drawing.Size(795, 108);
-            this.dataGridView2.TabIndex = 2;
+            this.Bcompra.Font = new System.Drawing.Font("Script MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bcompra.Location = new System.Drawing.Point(690, 613);
+            this.Bcompra.Name = "Bcompra";
+            this.Bcompra.Size = new System.Drawing.Size(179, 50);
+            this.Bcompra.TabIndex = 1;
+            this.Bcompra.Text = "Finalizar Compra";
+            this.Bcompra.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // BcancelarCompra
             // 
-            this.button1.Location = new System.Drawing.Point(644, 307);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 50);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Finalizar Compra";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BcancelarCompra.Font = new System.Drawing.Font("Script MT Bold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BcancelarCompra.Location = new System.Drawing.Point(894, 612);
+            this.BcancelarCompra.Name = "BcancelarCompra";
+            this.BcancelarCompra.Size = new System.Drawing.Size(148, 50);
+            this.BcancelarCompra.TabIndex = 2;
+            this.BcancelarCompra.Text = "Cancelar";
+            this.BcancelarCompra.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BcancelarCompra.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 13);
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(414, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(270, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "aca mostrar lo que agrega al \"carrito\"";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 440);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1033, 223);
-            this.dataGridView1.TabIndex = 1;
+            this.label1.Size = new System.Drawing.Size(102, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Producto";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 399);
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Productos";
+            this.label2.Size = new System.Drawing.Size(103, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cantidad";
+            // 
+            // Ncant
+            // 
+            this.Ncant.Location = new System.Drawing.Point(175, 164);
+            this.Ncant.Name = "Ncant";
+            this.Ncant.Size = new System.Drawing.Size(77, 26);
+            this.Ncant.TabIndex = 5;
+            // 
+            // CBtipoProd
+            // 
+            this.CBtipoProd.FormattingEnabled = true;
+            this.CBtipoProd.Location = new System.Drawing.Point(117, 86);
+            this.CBtipoProd.Name = "CBtipoProd";
+            this.CBtipoProd.Size = new System.Drawing.Size(280, 28);
+            this.CBtipoProd.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Script MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tipo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(304, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Venta";
+            // 
+            // CBproducto
+            // 
+            this.CBproducto.FormattingEnabled = true;
+            this.CBproducto.Location = new System.Drawing.Point(604, 86);
+            this.CBproducto.Name = "CBproducto";
+            this.CBproducto.Size = new System.Drawing.Size(298, 28);
+            this.CBproducto.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Script MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(414, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Precio Unitario";
+            // 
+            // TBprecioProd
+            // 
+            this.TBprecioProd.Location = new System.Drawing.Point(604, 168);
+            this.TBprecioProd.Name = "TBprecioProd";
+            this.TBprecioProd.Size = new System.Drawing.Size(298, 26);
+            this.TBprecioProd.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(604, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Script MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(414, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 29);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Subtotal";
+            // 
+            // BagregarCompra
+            // 
+            this.BagregarCompra.Font = new System.Drawing.Font("Script MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BagregarCompra.Location = new System.Drawing.Point(569, 299);
+            this.BagregarCompra.Name = "BagregarCompra";
+            this.BagregarCompra.Size = new System.Drawing.Size(179, 50);
+            this.BagregarCompra.TabIndex = 3;
+            this.BagregarCompra.Text = "Agregar";
+            this.BagregarCompra.UseVisualStyleBackColor = true;
+            // 
+            // DGcarrito
+            // 
+            this.DGcarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGcarrito.Location = new System.Drawing.Point(87, 436);
+            this.DGcarrito.Name = "DGcarrito";
+            this.DGcarrito.RowHeadersWidth = 62;
+            this.DGcarrito.RowTemplate.Height = 28;
+            this.DGcarrito.Size = new System.Drawing.Size(854, 150);
+            this.DGcarrito.TabIndex = 3;
             // 
             // FagregarVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::SaborAcielo.Properties.Resources.fondoSACinicio;
             this.ClientSize = new System.Drawing.Size(1082, 675);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DGcarrito);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Bcompra);
+            this.Controls.Add(this.BcancelarCompra);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FagregarVenta";
             this.Text = "FagregarVenta";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ncant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGcarrito)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Bcompra;
+        private System.Windows.Forms.Button BcancelarCompra;
+        private System.Windows.Forms.NumericUpDown Ncant;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TBprecioProd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CBproducto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CBtipoProd;
+        private System.Windows.Forms.Button BagregarCompra;
+        private System.Windows.Forms.DataGridView DGcarrito;
     }
 }
--- FlistarVentas.Designer.cs
+++ FlistarVentas.Designer.cs
@@ -28,130 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DGventas = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
+            this.Pventas = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CBfecha = new System.Windows.Forms.CheckBox();
+            this.CBcliente = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DGventas)).BeginInit();
+            this.Pventas.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DGventas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 313);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(995, 346);
-            this.dataGridView1.TabIndex = 0;
+            this.DGventas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGventas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGventas.Location = new System.Drawing.Point(27, 313);
+            this.DGventas.Name = "DGventas";
+            this.DGventas.RowHeadersWidth = 62;
+            this.DGventas.RowTemplate.Height = 28;
+            this.DGventas.Size = new System.Drawing.Size(995, 346);
+            this.DGventas.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(15, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.Size = new System.Drawing.Size(98, 27);
             this.label2.TabIndex = 2;
             this.label2.Text = "Empleado";
             // 
-            // panel1
+            // Pventas
             // 
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.dateTimePicker2);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.dateTimePicker1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(141, 36);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(597, 223);
-            this.panel1.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(135, 12);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(107, 106);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(117, 26);
-            this.dateTimePicker1.TabIndex = 4;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 106);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 20);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Desde";
+            this.Pventas.BackColor = System.Drawing.Color.RosyBrown;
+            this.Pventas.Controls.Add(this.groupBox1);
+            this.Pventas.Controls.Add(this.label3);
+            this.Pventas.Controls.Add(this.label2);
+            this.Pventas.Font = new System.Drawing.Font("Script MT Bold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pventas.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Pventas.Location = new System.Drawing.Point(84, 50);
+            this.Pventas.Name = "Pventas";
+            this.Pventas.Size = new System.Drawing.Size(851, 250);
+            this.Pventas.TabIndex = 3;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(296, 111);
+            this.label5.Location = new System.Drawing.Point(473, 36);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.Size = new System.Drawing.Size(68, 27);
             this.label5.TabIndex = 7;
             this.label5.Text = "Hasta";
             // 
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(365, 106);
+            this.dateTimePicker2.Location = new System.Drawing.Point(547, 36);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(117, 26);
+            this.dateTimePicker2.Size = new System.Drawing.Size(154, 34);
             this.dateTimePicker2.TabIndex = 6;
             // 
-            // label6
+            // label4
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(135, 80);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(240, 20);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Seleccione un periodo de tiempo";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Desde";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(289, 36);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 34);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(369, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 38);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Lista de ventas";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CBcliente);
+            this.groupBox1.Controls.Add(this.CBfecha);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(35, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 162);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtrar por";
+            // 
+            // CBfecha
+            // 
+            this.CBfecha.AutoSize = true;
+            this.CBfecha.Location = new System.Drawing.Point(28, 39);
+            this.CBfecha.Name = "CBfecha";
+            this.CBfecha.Size = new System.Drawing.Size(89, 31);
+            this.CBfecha.TabIndex = 8;
+            this.CBfecha.Text = "Fecha";
+            this.CBfecha.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CBfecha.UseVisualStyleBackColor = true;
+            // 
+            // CBcliente
+            // 
+            this.CBcliente.AutoSize = true;
+            this.CBcliente.Location = new System.Drawing.Point(28, 104);
+            this.CBcliente.Name = "CBcliente";
+            this.CBcliente.Size = new System.Drawing.Size(95, 31);
+            this.CBcliente.TabIndex = 9;
+            this.CBcliente.Text = "Cliente";
+            this.CBcliente.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CBcliente.UseVisualStyleBackColor = true;
             // 
             // FlistarVentas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SaborAcielo.Properties.Resources.fondoSACinicio;
             this.ClientSize = new System.Drawing.Size(1099, 671);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pventas);
+            this.Controls.Add(this.DGventas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FlistarVentas";
             this.Text = "FlistarVentas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGventas)).EndInit();
+            this.Pventas.ResumeLayout(false);
+            this.Pventas.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DGventas;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel Pventas;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox CBfecha;
+        private System.Windows.Forms.CheckBox CBcliente;
     }
 }
